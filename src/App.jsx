7532295445b51d9464bcb762f37896d85dd71690
sugurufukui <85082566+sugurufukui@users.noreply.github.com>
2022-05-22 22:25:35 +0900

    TODO５個制限設定

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,14 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTODO5個までです！消化してください！
+        </p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
